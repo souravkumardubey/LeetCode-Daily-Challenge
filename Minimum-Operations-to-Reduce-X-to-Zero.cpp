93a4c0cc8e47aa0d49945e7d47f9385d26ff1fab
Sourav Kumar Dubey <73059672+souravkumardubey@users.noreply.github.com>
2022-06-11 10:12:08 +0530

    11th June

---
+++ Minimum-Operations-to-Reduce-X-to-Zero.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        
+        int n = nums.size() , j = 0, sum = 0, maxlen = -1;
+        
+        int total = accumulate(begin(nums), end(nums), 0);
+        
+        if ( total < x ) {
+            
+            return -1;
+            
+        }
+        
+        for ( int i = 0 ; i < n; i++ ) {
+            
+            sum += nums[i];
+            
+            while ( sum > total - x )
+                sum -= nums[j++];
+            
+            if ( sum == total - x ) 
+                maxlen = max(maxlen, i - j + 1);
+            
+        }
+        
+        return maxlen == -1 ? -1 : n - maxlen;
+        
+    }
+};
